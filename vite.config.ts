4b84b6c296dde54b4227f8f6227ec736b671d25a
thiagosouza28 <thgdsztls@gmail.>
2025-08-18 17:35:34 -0300

    erro de tela branco corrigido

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // garante que todas as rotas funcionem no Vercel
-});
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+})
